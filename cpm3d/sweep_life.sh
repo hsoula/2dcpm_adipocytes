01f56a8fddfbc7be27bb513c5eb64bbde042387e
Hedi <hsoula@gmail.com>
2026-04-20 23:51:01 +0200

    bug time

--- cpm3d/sweep_life.sh
+++ cpm3d/sweep_life.sh
@@ -3,6 +3,6 @@ cargo run --release --bin simulate_life \
 -- --grid-w 60 --grid-h 60 --grid-d 60 \
 --target-volume 160 --volume-sigma 100  \
 --n-cells 2450 --lv 2.0 --ls 0.01 --li 0.01  \
---steps 10 --out-dir data/sim3d --save-every 100 \
+--steps 10000 --out-dir data/sim3d --save-every 100 \
 --death-rate 0.01 --birth-rate 0.2 --grow-rate 0.1 --seed $seed > data/out$seed.txt
 done
