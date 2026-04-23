e7969411f295290ca651eb900d028bbfb53db948
Hedi <hsoula@gmail.com>
2026-04-23 10:36:39 +0200

    extended params for sweep

--- cpm3d/sweep_life.sh
+++ cpm3d/sweep_life.sh
@@ -1,6 +1,6 @@
 # 1. run combinations
 for seed in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20; do
-  for grow in 0.001 0.01 0.1; do
+  for grow in 0.02 0.03 0.04 0.05 0.06 0.07 0.08 0.09; do
     for death in 0.01 0.02; do
       for birth in 0.005 0.01; do
         cargo run --release --bin simulate_life -- \
